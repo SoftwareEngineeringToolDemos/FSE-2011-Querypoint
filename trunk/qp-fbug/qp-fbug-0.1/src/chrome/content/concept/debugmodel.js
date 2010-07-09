7f7d28df3cb87c7de115026d6f1b3e66bb58c6c1
salman.mirghasemi <salman.mirghasemi@45a51ab6-8be0-ba9d-afe3-f39e2ce96256>
2010-07-09 21:22:58 +0000

    fbug

--- trunk/qp-fbug/qp-fbug-0.1/src/chrome/content/concept/debugmodel.js
+++ trunk/qp-fbug/qp-fbug-0.1/src/chrome/content/concept/debugmodel.js
@@ -1,8 +1,46 @@
-var DebugModelFactory = function{
+QPFBUG.const.querytypes.lastchange = 0;
+QPFBUG.const.querytypes.lastcondition = 1;
 
-  this.init = function(){
-       this.tracePoints = [];
-  }
+//--------------------------------- DebugModel --------------------------------
+QPFBUG.DebugModelFactory = function(){
+     this.tracePoints = [];
+     tracePointId = 0;
+}
 
-  
+QPFBUG.DebugModelFactory.prototype = {
+
+
+     addTracePoint() : function(){},
+
+
+
+
+}
+
+//------------------------------- TracePointDef -------------------------------
+// This object is the result of parsing the inserted query by the user
+QPFBUG.TracePointDefFactory = function(name){
+    //e.g., for "lastChange(A, foo), queryType is 0, refPoint is A, and refObj is foo.
+    this.name = name;
+    this.queryType = queryType;
+    this.refPoint = refPoint;
+    this.refObj = refObj;
+}
+
+QPFBUG.TracePointDefFactory.prototype = {
+    // no functions
+}
+
+
+//------------------------------- TracePoint ----------------------------------
+// This object is the live object is kept in debug model.
+QPFBUG.TracePointFactory = function(id, name, queryType){
+    this.id = id;
+    this.queryType = queryType;
+    this.refPoint = refPoint;
+    this.refObj = refObj;
+}
+
+QPFBUG.TracePointFactory.prototype = {
+
 }
