58030a2acb15fb07c4f0c550f4fb0f44157acac6
salman.mirghasemi <salman.mirghasemi@45a51ab6-8be0-ba9d-afe3-f39e2ce96256>
2010-07-11 01:23:10 +0000

    fbug

--- qp-fbug/qp-fbug-0.1/src/chrome/content/concept/qpfbugstate.js
+++ qp-fbug/qp-fbug-0.1/src/chrome/content/concept/qpfbugstate.js
@@ -1,23 +1,53 @@
+//var EXPORTED_SYMBOLS = ["QpfbugStateFactory","DebugSessionFactory", "ReproductionFactory"];
+
+//(function() {
 
 //--------------------------------- QpfbugState --------------------------------
+this.QpfbugStateFactory = function(){
 
-QPFBUG.QpfbugStateFactory = function(){
-
-   debugSessions = [];
+   this.nextDebugSessionId = 0;
+   this.debugSessions = [];
 }
 
 
-QPFBUG.QpfbugStateFactory.prototype = {
+this.QpfbugStateFactory.prototype =
+{
+   newDebugSession: function(){
+       var id = this.nextDebugSessionId++;
+       var debugSession = new this.DebugSessionFactory(id);
+       this.debugSessions.push(debugSession);
+       return debugSession;
+   },
 
-
+   getDebugSession: function(id){
+       for (i=0 ; i < this.debugSessions.length ; i++)
+       {
+           if (this.debugSessions[i].id == id)
+           {
+               return this.debugSessions[i];
+           }
+       }
+       return null;
+   }
 }
 
 //--------------------------------- DebugSession --------------------------------
-QPFBUG.DebugSessionFactory = function(){
+this.DebugSessionFactory = function(id){
+   this.id = id;
+}
+
+this.DebugSessionFactory.prototype = {
 
 }
 
-QPFBUG.DebugSessionFactory.prototype = {
+//--------------------------------- Reproduction --------------------------------
+this.ReproductionFactory = function(id){
+   this.id = id;
+}
+
+this.ReproductionFactory.prototype = {
 
 }
+
+//}).apply(QPFBUG);
 
