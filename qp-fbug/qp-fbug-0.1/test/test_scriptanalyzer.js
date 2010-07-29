b03664b246e2e2a12c35ad9989515ffd825d62f8
salman.mirghasemi <salman.mirghasemi@45a51ab6-8be0-ba9d-afe3-f39e2ce96256>
2010-07-29 01:27:32 +0000

    Simple script analyzer is added.

--- qp-fbug/qp-fbug-0.1/test/test_scriptanalyzer.js
+++ qp-fbug/qp-fbug-0.1/test/test_scriptanalyzer.js
@@ -3,62 +3,83 @@ function runTest()
     var QPFBUG = FBTest.FirebugWindow.QPFBUG;
     var Firebug = FBTest.FirebugWindow.Firebug;
 
+    var assignees;
+
     var script1 = "function(){}";
+
+    assignees = analyzeSource(script1);
+    FBTest.compare(0, assignees.length, "Script 1"); //
 
     var script2 = "function(){ "
          + "\n" + "    var foo; "
          + "\n" + "    foo.x[2] = {  "
-         + "\n" + "        bar : 1"
+         + "\n" + "        bar : {"
+         + "\n" + "            baz : 5"
+         + "\n" + "            }"
          + "\n" + "    }; "
          + "\n" + "    return foo;"
          + "\n" + "}"
 
-    analyzeSource(script1);
-    analyzeSource(script2);
+    assignees = analyzeSource(script2);
+    FBTest.compare(2, assignees.length, "Script 2"); //foo.x[2] & foo.x[2].bar
+    FBTest.compare("foo.x[2]", assignees[0], "Script 2");
+    FBTest.compare("foo.x[2].bar", assignees[1], "Script 2");
+
+    var script3 = "function(){ "
+         + "\n" + "    var foo; "
+         + "\n" + "    foo.x[2] = {  "
+         + "\n" + "        bar : function(){"
+         + "\n" + "            var baz = {"
+         + "\n" + "                foo2 : 5"
+         + "\n" + "            }"
+         + "\n" + "        }"
+         + "\n" + "    }; "
+         + "\n" + "    return foo;"
+         + "\n" + "}"
+
+    assignees = analyzeSource(script3);
+    FBTest.compare(2, assignees.length, "Script 3"); //foo.x[2] & baz
+    FBTest.compare("foo.x[2]", assignees[0], "Script 3");
+    FBTest.compare("baz", assignees[1], "Script 3");
+
+    var script4 = "function(){ "
+         + "\n" + "    var foo; "
+         + "\n" + "    foo.x[2] = {  "
+         + "\n" + "        bar : function(){"
+         + "\n" + "            var baz = {"
+         + "\n" + "                foo2 : {"
+         + "\n" + "                    foo2 : 5,"
+         + "\n" + "                    bar2 : function(){"
+         + "\n" + "                         baz2 = {"
+         + "\n" + "                             foo3: 5"
+         + "\n" + "                         }"
+         + "\n" + "                    }"
+         + "\n" + "                }"
+         + "\n" + "            }"
+         + "\n" + "        }"
+         + "\n" + "    }; "
+         + "\n" + "    return foo;"
+         + "\n" + "}"
+
+    assignees = analyzeSource(script4);
+    FBTest.compare(4, assignees.length, "Script 4");
+    FBTest.compare("foo.x[2]", assignees[0], "Script 4");
+    FBTest.compare("baz", assignees[1], "Script 4");
+    FBTest.compare("baz.foo2", assignees[2], "Script 4");
+    FBTest.compare("baz2", assignees[3], "Script 4");
+
+    assignees = analyzeSource(script4, true);
+    FBTest.compare(1, assignees.length, "Script 4 - Outer");
+    FBTest.compare("foo.x[2]", assignees[0], "Script 4 - Outer");
 
     FBTest.testDone();
 
 };
 
-function analyzeSource(source)
+function analyzeSource(source, onlyOuterScript)
 {
     var QPFBUG = FBTest.FirebugWindow.QPFBUG;
     var scriptAnalyzer = new QPFBUG.Classes.ScriptAnalyzer(source);
-    Node node = scriptAnalyzer.root;
-
-    var assignees = [];
-
-    var a = [];
-    a.push(node);
-    var
-    while (a.length > 0)
-    {
-        for (var i in this) {
-            if (this.hasOwnProperty(i) && i != 'type' && i != 'target')
-                a.push({id: i, value: this[i]});
-        }
-
-        if (node.type == scriptAnalyzer.jsParser.ASSIGN)
-        {
-             if (node[1] == scriptAnalyzer.jsParser.OBJECT_INIT)
-             {
-                node[0].getSource();
-             }
-        }
-
-
-
-        a.sort(function (a,b) { return (a.id < b.id) ? -1 : 1; });
-        const INDENTATION = "    ";
-        var n = ++Node.indentLevel;
-        var s = "{\n" + INDENTATION.repeat(n) + "type: " + tokenstr(this.type);
-        for (i = 0; i < a.length; i++)
-            s += ",\n" + INDENTATION.repeat(n) + a[i].id + ": " + a[i].value;
-        n = --Node.indentLevel;
-        s += "\n" + INDENTATION.repeat(n) + "}";
-        return s;
-    }
-
-
-    FBTrace.sysout("root", scriptAnalyzer.root);
+//    FBTrace.sysout("root", scriptAnalyzer.root);
+    return scriptAnalyzer.getObjectRefsToCreatedObjects(onlyOuterScript);
 }
