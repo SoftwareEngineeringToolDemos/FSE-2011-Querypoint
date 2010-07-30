c7081f435c02f653e2db9fd3f484b50ce1a09a5f
salman.mirghasemi <salman.mirghasemi@45a51ab6-8be0-ba9d-afe3-f39e2ce96256>
2010-07-30 20:14:54 +0000

    DebugService is added.

---
+++ trunk/qp-fbug/qp-fbug-0.1/src/chrome/content/di/executionmonitor.js
@@ -0,0 +1,51 @@
+var EXPORTED_SYMBOLS = ["loadModule"];
+loadModule = function(QPFBUG)
+{
+
+with (QPFBUG.Lang){
+with (QPFBUG.Classes){
+
+    var owner = QPFBUG.Classes;
+
+    //--------------------------- ExecutionMonitor ----------------------
+    owner.ExecutionMonitor = function(){
+
+        var constructor = function(fbs){
+            this.fbs = fbs;
+            this.dataStore = new DataStore();
+        };
+
+        constructor.prototype =
+        {
+            getReproduction: function(debugSession, reproductionId){
+                var reproduction;
+                if (reproductionId)
+                {
+                    reproduction = this.dataStore.getReproduction(reproductionId);
+                }
+                if (!reproduction)
+                {
+                    if (!debugSession)
+                        debugSession = this.dataStore.newDebugSession();
+                    reproduction = this.dataStore.newReproduction(debugSession);
+                    debugSession.reproductions.push(reproduction);
+                }
+                return reproduction;
+            },
+
+        };
+
+        constructor.getInstance = function(fbs){
+            if (!QPFBUG.manager)
+            {
+                QPFBUG.manager = new Manager(fbs);
+            }
+            return QPFBUG.manager;
+        };
+
+        return constructor;
+    }();
+
+}}
+
+};
